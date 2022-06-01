1838e8424c0336422f0601724bb2fb143ec7a4ea
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2022-06-01 01:52:20 +0000

    fix

--- src/sinks/aws_cloudwatch_logs/request.rs
+++ src/sinks/aws_cloudwatch_logs/request.rs
@@ -252,7 +252,7 @@ impl Client {
                 let owned_header = header.to_owned();
                 let owned_value = value.to_owned();
                 body.headers_mut().insert(
-                    http::HeaderValue::from_str(owned_header.as_str()).map_err(|err| {
+                    http::header::HeaderName::from_bytes(owned_header.as_bytes()).map_err(|err| {
                         aws_smithy_http::result::SdkError::ConstructionFailure(err.into())
                     })?,
                     http::HeaderValue::from_str(owned_value.as_str()).map_err(|err| {
